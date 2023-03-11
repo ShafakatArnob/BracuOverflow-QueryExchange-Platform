f35e8ada9752ee694694aa91481b36d11e88a870
ShafakatArnob <shafakatarnob98@gmail.com>
2023-03-11 20:08:14 +0600

    bug fix

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import authReducer from './state';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import { PersistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 
