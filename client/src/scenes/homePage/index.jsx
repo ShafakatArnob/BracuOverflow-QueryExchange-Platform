81f0edad799528a25ab38c4f987ef852e38c8036
ShafakatArnob <shafakatarnob98@gmail.com>
2023-04-28 22:01:23 +0600

    posts/ queries

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,8 +2,8 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
-// import MyPostWidget from "scenes/widgets/MyPostWidget";
-// import PostsWidget from "scenes/widgets/PostsWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
 // import AdvertWidget from "scenes/widgets/AdvertWidget";
 // import FriendListWidget from "scenes/widgets/FriendListWidget";
 
@@ -25,7 +25,7 @@ const HomePage = () => {
                 <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
                     <UserWidget userId={_id} picturePath={picturePath} />
                 </Box>
-                {/* <Box
+                <Box
                     flexBasis={isNonMobileScreens ? "42%" : undefined}
                     mt={isNonMobileScreens ? undefined : "2rem"}
                 >
@@ -34,11 +34,11 @@ const HomePage = () => {
                 </Box>
                 {isNonMobileScreens && (
                     <Box flexBasis="26%">
-                        <AdvertWidget />
+                        {/* <AdvertWidget /> */}
                         <Box m="2rem 0" />
-                        <FriendListWidget userId={_id} />
+                        {/* <FriendListWidget userId={_id} /> */}
                     </Box>
-                )} */}
+                )}
             </Box>
         </Box>
     );
